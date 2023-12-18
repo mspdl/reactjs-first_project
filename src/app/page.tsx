5433c9dd67b54970ec998318f6184cb6cbed8638
Morgan <morgan.espindola@gmail.com>
2023-12-18 14:42:50 -0300

    Basic component import and export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Square, { Circle } from "./components/Geo";
+
 const Page = () => {
   return (
     <div>
       <h1 className="font-bold text-2xl">Hello World!</h1>
       <h3>Another text</h3>
+
+      <Square />
+      <Circle />
     </div>
   );
 };
