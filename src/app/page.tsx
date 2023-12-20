27ff9d8b10370423b2eaec67f27dc3c4b5d5f5e6
Morgan <morgan.espindola@gmail.com>
2023-12-20 18:14:26 -0300

    Conditional Rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,13 @@ import PersonList from "../components/PersonList";
 const Page = () => {
   return (
     <div className="flex flex-col items-center max-w-7xl mt-4">
-      <Card>Text example</Card>
+      <Card phrase="Text example" author="Morgan">
+        ***
+      </Card>
+      <Card phrase="To be or not to be">*-*</Card>
       <AdvancedCard>
         <>
-          <h3 className="text-2xl italic text-center" >Text example</h3>
+          <h3 className="text-2xl italic text-center">Text example</h3>
           <p className="text-xs text-right">- Unkown author</p>
         </>
       </AdvancedCard>
--- src/components/SimpleCard.tsx
+++ src/components/SimpleCard.tsx
@@ -1,11 +1,16 @@
 type SimpleCardProps = {
   children: string;
+  phrase: string;
+  author?: string;
 };
 
-export const Card = ({ children }: SimpleCardProps) => {
+export const Card = ({ children, phrase, author }: SimpleCardProps) => {
   return (
-    <div className="border-2 border-red-400 p-3 text-3xl">
-      <p className="italic">"{children}"</p>
+    <div className="border-2 border-red-400 p-3 text-3xl m-1">
+      <p className="italic">
+        "{children} {phrase}"
+      </p>
+      <p className="text-xs text-right">{author ?? "Unkown author"}</p>
     </div>
   );
 };
