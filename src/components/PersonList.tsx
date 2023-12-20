5d371a8fc19803c83acb18bb4b8ce4c43d6a0287
Morgan <morgan.espindola@gmail.com>
2023-12-20 19:39:24 -0300

    Filtering lists

--- src/components/PersonList.tsx
+++ src/components/PersonList.tsx
@@ -5,10 +5,16 @@ import Person from "./Person";
 const PersonList = () => {
   return (
     <div className="flex items-center flex-col m-5">
-      <h1 className="font-bold text-lg">Person List</h1>
+      <h1 className="font-bold text-xl">Person List</h1>
       {personList.map(({ name, profession }: PersonType) => (
         <Person name={name} profession={profession} />
       ))}
+      <h2 className="font-bold text-lg">All the mathematicians</h2>
+      {personList
+        .filter((person) => person.profession === "mathematician")
+        .map((mathematician) => (
+          <p>{mathematician.name}</p>
+        ))}
     </div>
   );
 };
