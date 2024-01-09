aee526bb65fbecee5cd35bf24a5a9cd25953e154
Morgan <morgan.espindola@gmail.com>
2024-01-09 20:03:07 -0300

    timeline of states

--- src/components/StateExample.tsx
+++ src/components/StateExample.tsx
@@ -4,6 +4,7 @@ const StateExample = () => {
   const [showSecret, setShowSecret] = useState(false);
   const [nameInput, setNameInput] = useState("");
   const [showName, setShowName] = useState(false);
+  const [count, setCount] = useState(0);
 
   const handleButtonSecretAreaClick = () => {
     setShowSecret(!showSecret);
@@ -13,8 +14,29 @@ const StateExample = () => {
     setShowName(!showName);
   };
 
+  const handleCountButtonClick = () => {
+    console.log("count 0: ", count);
+    setCount(count + 2);
+    console.log("count 1: ", count);
+    setCount(count + 2);
+    console.log("count 2: ", count);
+    setCount(count + 2);
+    console.log("count 3: ", count);
+    console.log("new count: ", count + 2);
+    alert(count);
+  };
+
   return (
     <>
+      <div className="p-3 flex flex-col gap-2 items-center">
+        <p>{count}</p>
+        <button
+          onClick={handleCountButtonClick}
+          className="bg-blue-800 text-white p-3 rounded"
+        >
+          +2
+        </button>
+      </div>
       <input
         type="text"
         className="border border-black p-3 text-xl text-black rounded"
