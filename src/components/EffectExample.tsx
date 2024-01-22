2ec571c1b87f748d235179c682f30b57ead544fe
Morgan <morgan.espindola@gmail.com>
2024-01-22 14:40:29 -0300

    Right way of using useEffect

--- src/components/EffectExample.tsx
+++ src/components/EffectExample.tsx
@@ -1,40 +1,40 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const EffectExample = () => {
-  const [name, setName] = useState("Morgan");
+  const [firstName, setFirstName] = useState("Morgan");
+  const [lastName, setLastName] = useState("Espindola");
   const [age, setAge] = useState(34);
 
-  useEffect(() => {
-    console.log("Activated useEffect com EffectExample component.");
-  }, [age]);
+  const fullName = `${firstName} ${lastName}`;
 
   return (
-    <div className="w-[50%]">
-      <div className="mb-2 text-center text-2xl">
-        My name is <span className="font-bold">{name}</span> and I'm{" "}
-        <b>{age}</b> years old.
-      </div>
+    <div className="w-[40%]">
+      <p className="text-center text-xl">
+        My name is <b>{fullName}</b>.
+        <br />
+        I'm <b>{age}</b> year{age > 1 ? "s" : ""} old.
+      </p>
       <hr />
       <button
-        className="w-full border border-green-800 m-3 p-1 rounded block bg-green-300 text-green-800"
-        onClick={() => setName("Peter")}
+        className="w-full border border-green-800 my-3 p-1 rounded block bg-green-300 text-green-800"
+        onClick={() => setFirstName("Peter")}
       >
         Change name to Peter
       </button>
       <button
-        className="w-full border border-green-800 m-3 p-1 rounded block bg-green-300 text-green-800"
-        onClick={() => setName("John")}
+        className="w-full border border-green-800 my-3 p-1 rounded block bg-green-300 text-green-800"
+        onClick={() => setFirstName("John")}
       >
         Change name to John
       </button>
       <button
-        className="w-full border border-green-800 m-3 p-1 rounded block bg-green-300 text-green-800"
+        className="w-full border border-green-800 my-3 p-1 rounded block bg-green-300 text-green-800"
         onClick={() => setAge(10)}
       >
         Change age to 10
       </button>
       <button
-        className="w-full border border-green-800 m-3 p-1 rounded block bg-green-300 text-green-800"
+        className="w-full border border-green-800 my-3 p-1 rounded block bg-green-300 text-green-800"
         onClick={() => setAge(28)}
       >
         Change age to 28
