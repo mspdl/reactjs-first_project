0f88bd6f41035df166e1ef307c4066c47bef7848
Morgan <morgan.espindola@gmail.com>
2024-02-11 16:43:43 -0300

    Doing GET with Axios

--- src/components/Axios.tsx
+++ src/components/Axios.tsx
@@ -1,3 +1,33 @@
+import axios from "axios";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
 export const Axios = () => {
-  return <div className="">Axios</div>
-}
+  const currentPostId = 3;
+
+  const handleClickGetPosts = async () => {
+    const requestParams = { postId: currentPostId, sort: "desc" };
+
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/comments",
+      {
+        params: requestParams,
+      }
+    );
+
+    console.log(response.data);
+  };
+
+  return (
+    <div className="">
+      <button onClick={handleClickGetPosts}>
+        Get comments from post {currentPostId}
+      </button>
+    </div>
+  );
+};
