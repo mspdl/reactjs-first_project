dcce0f01f89f03c7360b0255af0e63acff319fbf
Morgan <morgan.espindola@gmail.com>
2024-02-12 11:47:50 -0300

    Now a post request

--- src/components/Axios.tsx
+++ src/components/Axios.tsx
@@ -23,10 +23,36 @@ export const Axios = () => {
     console.log(response.data);
   };
 
+  const handleAddPost = async () => {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/posts",
+      {
+        userId: 1,
+        title: "Post title",
+        body: "This is the body of the post",
+      }
+    );
+    console.log(response);
+    if (response.data.id) {
+      console.log("Post added successfully!");
+    } else {
+      console.log("Something went wrong.");
+    }
+  };
+
   return (
-    <div className="">
-      <button onClick={handleClickGetPosts}>
+    <div className="flex flex-col gap-3">
+      <button
+        className="bg-blue-700 hover:bg-blue-900 hover:text-gray-400 p-2 text-gray-300 rounded-md border border-gray-300"
+        onClick={handleClickGetPosts}
+      >
         Get comments from post {currentPostId}
+      </button>
+      <button
+        className="bg-blue-700 hover:bg-blue-900 hover:text-gray-400 p-2 text-gray-300 rounded-md border border-gray-300"
+        onClick={handleAddPost}
+      >
+        Add post
       </button>
     </div>
   );
