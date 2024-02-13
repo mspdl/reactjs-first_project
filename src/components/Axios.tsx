34160c3ac3542c0d48c10b817c73efbfad946d7b
Morgan <morgan.espindola@gmail.com>
2024-02-13 17:46:19 -0300

    Sending files with axios

--- src/components/Axios.tsx
+++ src/components/Axios.tsx
@@ -1,7 +1,9 @@
 import { api } from "@/utils/api";
+import { useRef } from "react";
 
 export const Axios = () => {
   const currentPostId = 3;
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleGetComments = async () => {
     const requestParams = { postId: currentPostId, sort: "desc" };
@@ -27,6 +29,18 @@ export const Axios = () => {
     }
   };
 
+  const handleSendFile = async () => {
+    if (fileInputRef.current?.files && fileInputRef.current.files.length > 0) {
+      const file = fileInputRef.current.files[0];
+      const data = new FormData();
+      data.append("name", "Morgan");
+      data.append("file", file);
+
+      const response = await api.post("/files", data);
+      console.log(response.data);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-3">
       <button
@@ -40,6 +54,14 @@ export const Axios = () => {
         onClick={handleAddPost}
       >
         Add post
+      </button>
+      <hr />
+      <input type="file" ref={fileInputRef} />
+      <button
+        className="bg-blue-700 hover:bg-blue-900 hover:text-gray-400 p-2 text-gray-300 rounded-md border border-gray-300"
+        onClick={handleSendFile}
+      >
+        Send File
       </button>
     </div>
   );
