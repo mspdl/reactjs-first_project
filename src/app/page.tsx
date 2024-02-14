e6e1a549efecad309305c4da60c0b73daa7cfc8b
Morgan <morgan.espindola@gmail.com>
2024-02-14 08:12:07 -0300

    Install TanStack and config Provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { Axios } from "@/components/Axios";
+import { TanStack } from "@/components/TanStack";
 
 const Page = () => {
   return (
     <div className="min-w-screen min-h-screen px-5 flex flex-col gap-2 justify-center items-center bg-gray-600">
-      <Axios />
+      <TanStack />
     </div>
   );
 };
---
+++ src/components/TanStack.tsx
@@ -0,0 +1,7 @@
+export const TanStack = () => {
+  return (
+    <div className="">
+      <h1 className="text-white text-3xl">Hello world</h1>
+    </div>
+  );
+};
