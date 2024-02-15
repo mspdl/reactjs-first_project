0e689068e1ded9f7e638939cb89826d103060acc
Morgan <morgan.espindola@gmail.com>
2024-02-15 08:18:48 -0300

    TanStack Devtools

--- src/utils/providers.tsx
+++ src/utils/providers.tsx
@@ -1,6 +1,7 @@
-"use client"
+"use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode } from "react";
 
 type ProvidersProps = {
@@ -11,6 +12,13 @@ export const Providers = ({ children }: ProvidersProps) => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools
+        initialIsOpen={false}
+        position="bottom"
+        buttonPosition="bottom-right"
+      />
+    </QueryClientProvider>
   );
 };
