b8e76f72c5a158132eaff27586ac08bfbde662d7
Morgan <morgan.espindola@gmail.com>
2024-02-15 08:36:46 -0300

    Creating and requesting a query

--- src/components/TanStack.tsx
+++ src/components/TanStack.tsx
@@ -1,7 +1,28 @@
+import { Post } from "@/types/Post";
+import { api } from "@/utils/api";
+import { useQuery } from "@tanstack/react-query";
+
 export const TanStack = () => {
+  const query = useQuery({
+    queryKey: ["posts"],
+    queryFn: async (): Promise<Post[]> => {
+      const result = await api.get("posts");
+      return result.data;
+    },
+  });
+
   return (
     <div className="">
-      <h1 className="text-white text-3xl">Hello world</h1>
+      <h1 className="text-white text-3xl text-center">Hello world</h1>
+
+      {query.isLoading && "Loading..."}
+      {query.data && (
+        <ul>
+          {query.data.map((post: Post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
