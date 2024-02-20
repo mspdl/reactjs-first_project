aa4bcc472ae1168cf88d8d5a5ce316b5a77874b1
Morgan <morgan.espindola@gmail.com>
2024-02-20 07:07:50 -0300

    Controlling SlateTime

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -2,12 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import { getPostById, getPosts } from "./api";
 
 export const usePosts = () => {
+  const ONE_MINUTE = 60 * 1000;
+
   return useQuery({
     networkMode: "online",
-      // default: online
-      // always: always execute the function (recommended for projects without API)
+    // networkMode: default = online
+    // networkMode: always = always execute the function (recommended for projects without API)
     queryKey: ["posts"],
     queryFn: getPosts,
+    staleTime: 5 * ONE_MINUTE,
+    // slateTime: Infinity = the request will be always fresh, it'll never be slate 
   });
 };
 
