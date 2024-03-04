8229d5c0cfb9b61eb77874ef5d930a34f5c686ad
Morgan <morgan.espindola@gmail.com>
2024-03-04 14:27:49 -0300

    Invaliding by Mutation

--- src/utils/mutations.ts
+++ src/utils/mutations.ts
@@ -1,8 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
 import { addPost } from "./api";
+import { queryClient } from "./queryClient";
 
 export const useAddPost = () => {
   return useMutation({
     mutationFn: addPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["/posts"],
+      });
+    },
   });
 };
