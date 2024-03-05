6f94aff918c86f12b78d84f28174d6f70f82d0e2
Morgan <morgan.espindola@gmail.com>
2024-03-05 08:04:19 -0300

    Creating a basic form

--- src/components/ReactFormExamples.tsx
+++ src/components/ReactFormExamples.tsx
@@ -1,7 +1,40 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  lastName: string;
+};
+
 export const ReactFormExamples = () => {
+  const { handleSubmit, register } = useForm<Inputs>();
+
+  const handleFormSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="">
-      <p className="font-bold text-2xl">React Form</p>
+      <p className="font-bold text-3xl text-center">React Form</p>
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <input
+          className="block border p-3 border-white rounded-md mt-3 text-black"
+          type="text"
+          {...register("name")}
+          placeholder="Type your name"
+        />
+        <input
+          className="block border p-3 border-white rounded-md mt-3 text-black"
+          type="text"
+          {...register("lastName")}
+          placeholder="Type your last name"
+        />
+
+        <input
+          className="border p-3 cursor-pointer border-white mt-3 bg-blue-700 hover:bg-blue-900 rounded-md w-full text-white text-xl"
+          type="submit"
+          value="Send"
+        />
+      </form>
     </div>
   );
 };
