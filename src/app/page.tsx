0bbfc9dc835400788f8af0c50301e2619f05cb28
Morgan <morgan.espindola@gmail.com>
2024-03-05 07:51:22 -0300

    React Form Introduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { TanStack } from "@/components/TanStack";
+import { ReactFormExamples } from "@/components/ReactFormExamples";
 
 const Page = () => {
   return (
     <div className="min-w-screen min-h-screen px-5 flex flex-col gap-2 justify-center items-center bg-gray-600">
-      <TanStack />
+      <ReactFormExamples />
     </div>
   );
 };
---
+++ src/components/ReactFormExamples.tsx
@@ -0,0 +1,7 @@
+export const ReactFormExamples = () => {
+  return (
+    <div className="">
+      <p className="font-bold text-2xl">React Form</p>
+    </div>
+  );
+};
