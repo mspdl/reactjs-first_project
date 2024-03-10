a68ef64159f0d4abce0786c0e6ac1c52854b1434
Morgan <morgan.espindola@gmail.com>
2024-03-10 18:04:45 -0300

    Integrating with UI libraries

--- src/components/ReactFormExamples.tsx
+++ src/components/ReactFormExamples.tsx
@@ -1,9 +1,9 @@
 import { SignUpForm } from "@/types/SignUpForm";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { Input } from "./Input";
+import { Input } from "@mui/material";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 
 export const ReactFormExamples = () => {
-  const { control, handleSubmit, setValue } = useForm<SignUpForm>();
+  const { control, handleSubmit } = useForm<SignUpForm>();
 
   const handleFormSubmit: SubmitHandler<SignUpForm> = (data) => {
     console.log(data);
@@ -12,29 +12,48 @@ export const ReactFormExamples = () => {
   return (
     <div className="">
       <p className="font-bold text-3xl text-center">React Form</p>
-      <form onSubmit={handleSubmit(handleFormSubmit)}>
-        <Input
+      <form onSubmit={handleSubmit(handleFormSubmit)} className="flex flex-col center">
+        <Controller
           control={control}
           name="name"
+          render={({ field, fieldState }) => (
+            <Input
+              {...field}
+              error={fieldState.invalid}
+              style={{ backgroundColor: "#FFF", padding: 2, margin: 2 }}
+            />
+          )}
           rules={{ required: true, minLength: 2, maxLength: 20 }}
         />
-
-        <Input control={control} name="lastName" />
-
-        <Input
+        <Controller
+          control={control}
+          name="lastName"
+          render={({ field, fieldState }) => (
+            <Input
+              {...field}
+              error={fieldState.invalid}
+              style={{ backgroundColor: "#FFF", padding: 2, margin: 2 }}
+            />
+          )}
+        />
+        <Controller
           control={control}
           name="age"
-          rules={{ required: true, min: 18, max: 120 }}
+          render={({ field, fieldState }) => (
+            <Input
+              {...field}
+              error={fieldState.invalid}
+              style={{ backgroundColor: "#FFF", padding: 2, margin: 2 }}
+            />
+          )}
+          rules={{ required: true, minLength: 18 }}
         />
-
         <input
           className="border p-3 cursor-pointer border-white mt-3 bg-blue-700 hover:bg-blue-900 rounded-md w-full text-white text-xl"
           type="submit"
           value="Send"
         />
       </form>
-
-      <button onClick={() => setValue('age', 1)}>Define as adult</button>
     </div>
   );
 };
