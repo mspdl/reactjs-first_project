e6a2d2b8b7bd8fe6876778518807db162bd21bad
Morgan <morgan.espindola@gmail.com>
2024-03-17 23:45:58 -0300

    Defining error message using Zod

--- src/components/ZodExample.tsx
+++ src/components/ZodExample.tsx
@@ -3,13 +3,24 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const SignUpFormSchema = z.object({
-  name: z.string().min(2).max(20),
+  name: z
+    .string()
+    .min(2, { message: "The name must have at least 2 characters" })
+    .max(20),
   lastName: z.string().optional(),
-  age: z.number().min(18).max(130),
+  email: z.string().email(),
+  age: z
+    .number({ invalid_type_error: "Age needs to be a number" })
+    .min(18)
+    .max(130),
 });
 
 export const ZodExample = () => {
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: zodResolver(SignUpFormSchema),
   });
 
@@ -22,24 +33,54 @@ export const ZodExample = () => {
       <h1 className="p-3 text-2xl font-bold text-white text-center">
         SignUp Form
       </h1>
-      <form onSubmit={handleSubmit(handleSignUpForm)} className="flex flex-col">
-        <input
-          {...register("name")}
-          className="border-white p-3 mb-3 text-black"
-        />
-        <input
-          {...register("lastName")}
-          className="border-white p-3 mb-3 text-black"
-        />
-        <input
-          {...register("age", { valueAsNumber: true })}
-          className="border-white p-3 mb-3 text-black"
-        />
-        <input
-          type="submit"
-          value="Register"
-          className="p-3 mt-3 font-bold bg-blue-600 text-white rounded-md"
-        />
+      <form onSubmit={handleSubmit(handleSignUpForm)}>
+        <div className="mb-3">
+          <input
+            placeholder="First Name"
+            {...register("name")}
+            className="border-white p-3  text-black"
+          />
+          {errors.name && (
+            <p className="text-red-400">{errors.name.message as string}</p>
+          )}
+        </div>
+        <div className="mb-3">
+          <input
+            placeholder="Last Name"
+            {...register("lastName")}
+            className="border-white p-3 text-black"
+          />
+          {errors.lastName && (
+            <p className="text-red-400">{errors.lastName.message as string}</p>
+          )}
+        </div>
+        <div className="mb-3">
+          <input
+            placeholder="E-mail"
+            {...register("email")}
+            className="border-white p-3 text-black"
+          />
+          {errors.email && (
+            <p className="text-red-400">{errors.email.message as string}</p>
+          )}
+        </div>
+        <div className="mb-3">
+          <input
+            placeholder="Age"
+            {...register("age", { valueAsNumber: true })}
+            className="border-white p-3 text-black"
+          />
+          {errors.age && (
+            <p className="text-red-400">{errors.age.message as string}</p>
+          )}
+        </div>
+        <div>
+          <input
+            type="submit"
+            value="Register"
+            className="p-3 mt-3 font-bold bg-blue-600 text-white rounded-md"
+          />
+        </div>
       </form>
     </div>
   );
